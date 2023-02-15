a92afb8214efa02eabd07c0a6042987088f1df85
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2023-02-15 08:42:52 -0300

    Sistema para cálculo de média concluído

--- aula 07/exercicio1/app.js
+++ aula 07/exercicio1/app.js
@@ -17,71 +17,113 @@ var entradaDados = readLine.createInterface({
 entradaDados.question('Nome do aluno (a):\n', function (aluno) {
     let nomeAluno = aluno
 
-    entradaDados.question('Digite o sexo do (a) aluno (a):\n1 - Feminino\n2 - Masculino:\n', function (sexo1) {
+    entradaDados.question('Digite o número referente ao sexo do (a) aluno (a) :\n1 - Feminino\n2 - Masculino:\n', function (sexo1) {
         let sexoAluno = sexo1
+        let generoAluno = calculoMedia.sexAluno(sexoAluno)
+        if (generoAluno == 'erro') {
+            entradaDados.close()
+        } else {
 
-        entradaDados.question('Nome do (a) professor (a):\n', function (professor) {
-            let nomeProfessor = professor
+            entradaDados.question('Nome do (a) professor (a):\n', function (professor) {
+                let nomeProfessor = professor
 
-            entradaDados.question('Digite o sexo do (a) professor (a): \n1 - Feminino\n2 - Masculino:\n', function (sexo2) {
-                let sexoProfessor = sexo2
+                entradaDados.question('Digite o número referente ao sexo do (a) professor (a): \n1 - Feminino\n2 - Masculino:\n', function (sexo2) {
+                    let sexoProfessor = sexo2
+                    let generoProfessor = calculoMedia.sexProfessor(sexoProfessor)
+                    if (generoProfessor == 'erro') {
+                        entradaDados.close()
+                    } else {
 
-                entradaDados.question('Digite o nome do curso:\n', function (curso) {
-                    let nomeCurso = curso
+                        entradaDados.question('Digite o nome do curso:\n', function (curso) {
+                            let nomeCurso = curso
 
-                    entradaDados.question('Digite a disciplina do curso:\n', function (disciplina) {
-                        let nomeDisciplina = disciplina
+                            entradaDados.question('Digite a disciplina do curso:\n', function (disciplina) {
+                                let nomeDisciplina = disciplina
 
-                        //Entrada de notas
-                        entradaDados.question('Digite a primeira nota:\n', function (primeiraNota) {
-                            let valor1 = primeiraNota
+                                //Entrada de notas
+                                entradaDados.question('Digite a primeira nota:\n', function (primeiraNota) {
+                                    let valor1 = primeiraNota
 
-                            entradaDados.question('Digite a segunda nota:\n', function (segundaNota) {
-                                let valor2 = segundaNota
+                                    entradaDados.question('Digite a segunda nota:\n', function (segundaNota) {
+                                        let valor2 = segundaNota
 
-                                entradaDados.question('Digite a terceira nota:\n', function (terceiraNota) {
-                                    let valor3 = terceiraNota
+                                        entradaDados.question('Digite a terceira nota:\n', function (terceiraNota) {
+                                            let valor3 = terceiraNota
 
-                                    entradaDados.question('Digite a quarta nota:\n', function (quartaNota) {
-                                        let valor4 = quartaNota
-                                        let resultado
+                                            entradaDados.question('Digite a quarta nota:\n', function (quartaNota) {
+                                                let valor4 = quartaNota
 
-                                        let generoAluno = calculoMedia.sexAluno(sexoAluno)
-                                        let generoProfessor = calculoMedia.sexProfessor(sexoProfessor)
+                                                if (nomeAluno == '' || nomeProfessor == '' || nomeCurso == '' || nomeDisciplina == '') {
+                                                    console.log('Nenhum dado pode estar vazio.')
+                                                    entradaDados.close()
+                                                } else {
+                                                    let resultado
+                                                    let statusAluno
 
-                                        resultado = calculoMedia.calcularMedia(sexoAluno, valor1, valor2, valor3, valor4)
+                                                    resultado = calculoMedia.calcularMedia(valor1, valor2, valor3, valor4)
 
-                                        //exame
-                                        if (resultado == 'exame') {
-                                            entradaDados.question('\nVocê ficou de exame.\nDigite a nota do exame:\n', function (exame) {
-                                                let notaExame = exame
-                                                let exameNecessario
-                                                exameNecessario = calculoMedia.calcularMediaExame(sexoAluno, valor1, valor2, valor3, valor4, notaExame)
+                                                    if (resultado == 'erro') {
+                                                        entradaDados.close()
+                                                    } else {
+                                                        if (resultado >= 70) {
+                                                            statusAluno = 'aprovad'
+                                                        } else if (resultado >= 50 && resultado <= 69) {
+                                                            statusAluno = 'exame'
+                                                        } else {
+                                                            statusAluno = 'reprovad'
+                                                        }
 
-                                                console.log('Alun' + calculoMedia.sexAluno(sexoAluno) + ' ' + exameNecessario + calculoMedia.sexAluno(sexoAluno))
+                                                        //exame
+                                                        if (statusAluno == 'exame') {
+                                                            entradaDados.question('\nVocê ficou de exame.\nDigite a nota do exame:\n', function (exame) {
+                                                                let notaExame = exame
+                                                                let exameNecessario
+                                                                exameNecessario = calculoMedia.calcularMediaExame(valor1, valor2, valor3, valor4, notaExame)
+
+                                                                if (exameNecessario == 'erro') {
+                                                                    entradaDados.close()
+                                                                } else {
+                                                                    if (exameNecessario < 60) {
+                                                                        statusAluno = 'reprovad'
+                                                                    } else {
+                                                                        statusAluno = 'aprovad'
+                                                                    }
+
+                                                                    console.log('\n------------------RELATÓRIO------------------\n')
+                                                                    console.log('Alun' + generoAluno + ' ' + nomeAluno + ' ' + statusAluno + generoAluno + ' na disciplina ' + nomeDisciplina)
+                                                                    console.log('Curso: ' + nomeCurso)
+                                                                    console.log(generoProfessor + nomeProfessor)
+                                                                    console.log('Notas d' + generoAluno + ' alun' + generoAluno + ': ' + valor1 + ', ' + valor2 + ', ' + valor3 + ', ' + valor4 + ', ' + notaExame)
+                                                                    console.log('Média final: ' + resultado)
+                                                                    console.log('Média com exame: ' + exameNecessario)
+                                                                }
+                                                            })
+                                                        } else {
+                                                            console.log('\n------------------RELATÓRIO------------------\n')
+                                                            console.log('Alun' + generoAluno + ' ' + nomeAluno + ' ' + statusAluno + generoAluno + ' na disciplina ' + nomeDisciplina)
+                                                            console.log('Curso: ' + nomeCurso)
+                                                            console.log(generoProfessor + nomeProfessor)
+                                                            console.log('Notas d' + generoAluno + ' alun' + generoAluno + ': ' + valor1 + ', ' + valor2 + ', ' + valor3 + ', ' + valor4)
+                                                            console.log('Média final: ' + resultado)
+                                                        }
+                                                    }
+
+
+                                                }
+
+
                                             })
-
-                                        } else {
-                                            console.log('\n------------------RELATÓRIO------------------\n')
-                                            console.log('Alun' + generoAluno + ' ' + nomeAluno + ' ' + resultado + generoAluno + ' na disciplina ' + nomeDisciplina)
-                                            console.log('Curso: ' + nomeCurso)
-                                            console.log(generoProfessor + nomeProfessor)
-                                            console.log('Notas d' + generoAluno + ' alun' + generoAluno + ': ' + valor1 + ', ' + valor2 + ', ' + valor3 + ', ' + valor4)
-                                            //arrumar:
-                                            console.log('Média final: ' + calculoMedia.calcularMedia.resultado)
-
-                                        }
+                                        })
                                     })
                                 })
                             })
                         })
-                    })
+                    }
                 })
             })
-        })
+        }
     })
 })
-
 
 //
 
--- aula 07/exercicio1/modulo/calculoMedia.js
+++ aula 07/exercicio1/modulo/calculoMedia.js
@@ -5,45 +5,35 @@
  * Versão: 1.0  
  **************************************************************************************/
 
-const calcularMedia = function (generoAluno, primeiraNota, segundaNota, terceiraNota, quartaNota) {
-    let sexoDoAluno = generoAluno
-    let nota1 = Number(String(primeiraNota))
-    let nota2 = Number(String(segundaNota))
-    let nota3 = Number(String(terceiraNota))
-    let nota4 = Number(String(quartaNota))
-    let statusAluno
 
+const calcularMedia = function (primeiraNota, segundaNota, terceiraNota, quartaNota) {
+    let nota1 = Number(primeiraNota)
+    let nota2 = Number(segundaNota)
+    let nota3 = Number(terceiraNota)
+    let nota4 = Number(quartaNota)
 
     if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0 || nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100) {
         console.log('ERRO: Digite uma nota válida (entre 0 e 100).')
+        return 'erro'
     } else if (nota1 === '' || nota2 === '' || nota3 === '' || nota4 === '') {
-        console.log('ERRO: Digite um valor. snfajfhhj')
+        console.log('ERRO: Digite um valor.')
+        return 'erro'
     } else if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)) {
         console.log('ERRO: Digite um valor válido')
+        return 'erro'
     } else {
         let resultado = (nota1 + nota2 + nota3 + nota4) / 4
 
-        if (resultado >= 70) {
-            statusAluno = 'aprovad'
-        } else if (resultado >= 50 && resultado <= 69) {
-            statusAluno = 'exame'           
-        } else {
-            statusAluno = 'reprovad'          
-        }
-
-        return statusAluno
+        return resultado
     }
 }
 
-const calcularMediaExame = function (generoAluno, primeiraNota, segundaNota, terceiraNota, quartaNota, exame) {
-    let sexoDoAluno = generoAluno
-    let nota1 = Number(String(primeiraNota))
-    let nota2 = Number(String(segundaNota))
-    let nota3 = Number(String(terceiraNota))
-    let nota4 = Number(String(quartaNota))
-    let notaExame = Number(String(exame))
-    let statusAluno
-
+const calcularMediaExame = function (primeiraNota, segundaNota, terceiraNota, quartaNota, exame) {
+    let nota1 = Number(primeiraNota)
+    let nota2 = Number(segundaNota)
+    let nota3 = Number(terceiraNota)
+    let nota4 = Number(quartaNota)
+    let notaExame = Number(exame)
 
     let mediaComExame
 
@@ -51,43 +41,62 @@ const calcularMediaExame = function (generoAluno, primeiraNota, segundaNota, ter
 
     if (notaExame > 100 || notaExame < 0) {
         console.log('ERRO: Digite um valor válido')
+        return 'erro'
     } else if (notaExame === '') {
         console.log('ERRO: Digite uma nota válida.')
+        return 'erro'
     } else if (isNaN(notaExame)) {
         console.log('ERRO: Digite um valor válido.')
+        return 'erro'
     } else {
-        if (mediaComExame < 60) {
-            statusAluno = 'reprovad'
-            return statusAluno
-        } else {
-            statusAluno = 'aprovad'
-            return statusAluno
-        }
+        return mediaComExame
     }
-
 }
 
 const sexAluno = function (generoAluno) {
     let sexoAluno = generoAluno
 
-    if (sexoAluno == 1) {
-        sexoAluno = 'a'
-        return sexoAluno
+    if (sexoAluno > 2) {
+        console.log('ERRO: Digite um número válido (1 ou 2)')
+        return 'erro'
+    } else if (sexoAluno < 1) {
+        console.log('ERRO: Digite um número válido (1 ou 2)')
+        return 'erro'
+    } else if (isNaN(sexoAluno)) {
+        console.log('ERRO: Digite um **número** válido (1 ou 2)')
+        return 'erro'
     } else {
-        sexoAluno = 'o'
-        return sexoAluno
+        if (sexoAluno == 1) {
+            sexoAluno = 'a'
+            return sexoAluno
+        } else {
+            sexoAluno = 'o'
+            return sexoAluno
+        }
     }
 }
+
 
 const sexProfessor = function (generoProfessor) {
     let sexoProfessor = generoProfessor
 
-    if (sexoProfessor == 1) {
-        sexoProfessor = 'Professora: '
-        return sexoProfessor
+    if (sexoProfessor > 2) {
+        console.log('ERRO: Digite um número válido (1 ou 2)')
+        return 'erro'
+    } else if (sexoProfessor < 1) {
+        console.log('ERRO: Digite um número válido (1 ou 2)')
+        return 'erro'
+    } else if (isNaN(sexoProfessor)) {
+        console.log('ERRO: Digite um **número** válido (1 ou 2)')
+        return 'erro'
     } else {
-        sexoProfessor = 'Professor: ';
-        return sexoProfessor;
+        if (sexoProfessor == 1) {
+            sexoProfessor = 'Professora: '
+            return sexoProfessor
+        } else {
+            sexoProfessor = 'Professor: ';
+            return sexoProfessor;
+        }
     }
 }
 
